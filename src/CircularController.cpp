1e2fd1644b7faf5699d1d79ecfdbdf292da315d7
soucsouc <martin.soucail@student-cs.fr>
2024-06-26 18:49:19 +0900

    circular moving with velocity and acceleration references

--- src/CircularController.cpp
+++ src/CircularController.cpp
@@ -1,19 +1,86 @@
 #include "CircularController.h"
+#include <Eigen/src/Core/Matrix.h>
+#include <SpaceVecAlg/PTransform.h>
+#include <cmath>
+#include <ctime>
+#include <mc_rbdyn/RobotLoader.h>
+#include <mc_tasks/EndEffectorTask.h>
+#include <mc_tasks/PostureTask.h>
+#include <mc_tasks/OrientationTask.h>
+
 
 CircularController::CircularController(mc_rbdyn::RobotModulePtr rm, double dt, const mc_rtc::Configuration & config)
-: mc_control::MCController(rm, dt)
+: mc_control::MCController(rm, dt, config, Backend::TVM)
 {
+  start_moving_ = false;
+  ctlTime_=0;
+  dynamicsConstraint = mc_rtc::unique_ptr<mc_solver::DynamicsConstraint>(new mc_solver::DynamicsConstraint(robots(), 0, solver().dt(), {0.1, 0.01, 0.5}, 0.9, false, true));
+  config_.load(config);
   solver().addConstraintSet(contactConstraint);
-  solver().addConstraintSet(kinematicsConstraint);
-  solver().addTask(postureTask);
-  solver().setContacts({{}});
+  solver().addConstraintSet(dynamicsConstraint);
 
-  mc_rtc::log::success("CircularController init done ");
+  R_=0.15;
+  omega_= 2;
+
+  postureTask = std::make_shared<mc_tasks::PostureTask>(solver(), robot().robotIndex(), 5, 1);
+  // postureTask->stiffness(3);
+  // postureTask->damping(5);
+  solver().addTask(postureTask);
+
+
+  circularTask = std::make_shared<mc_tasks::EndEffectorTask>(robot().frame("tool_frame"), 20.0, 500);
+  Eigen::VectorXd dimweight(6); 
+  dimweight << 1., 1., 1., 1., 1., 1. ; 
+  circularTask -> dimWeight(dimweight);
+  circularTask->reset();
+
+  solver().addTask(circularTask);
+
+  datastore().make<std::string>("ControlMode", "Torque"); // entree dans le datastore
+  datastore().make_call("getPostureTask", [this]() -> mc_tasks::PostureTaskPtr { return postureTask; });
+
+  gui()->addElement(this, {"Control Mode"},
+                    mc_rtc::gui::Label("Current Control :", [this]() { return this->datastore().get<std::string>("ControlMode"); }),
+                    mc_rtc::gui::Button("Position", [this]() { datastore().assign<std::string>("ControlMode", "Position"); }),
+                    mc_rtc::gui::Button("Torque", [this]() { datastore().assign<std::string>("ControlMode", "Torque"); }));
+
+  gui()->addElement({"Tasks"},
+    mc_rtc::gui::Checkbox("Circular Moving", this->start_moving_)
+  );
+
+  logger().addLogEntry("ControlMode",
+                       [this]()
+                       {
+                         auto mode = datastore().get<std::string>("ControlMode");
+                         if(mode.compare("") == 0) return 0;
+                         if(mode.compare("Position") == 0) return 1;
+                         if(mode.compare("Velocity") == 0) return 2;
+                         if(mode.compare("Torque") == 0) return 3;
+                         return 0;
+                       });
+
+  mc_rtc::log::success("CircularController init done");
 }
 
 bool CircularController::run()
-{
-  return mc_control::MCController::run();
+{ 
+  ctlTime_ += timeStep;
+  if (start_moving_) { circularTask->positionTask->position(Eigen::Vector3d(0.55, R_*std::sin(omega_*ctlTime_), 0.4 + R_*std::cos(omega_*ctlTime_))),
+    circularTask->positionTask->refVel(Eigen::Vector3d(0, R_*omega_*std::cos(omega_*ctlTime_), -R_*omega_*std::sin(omega_*ctlTime_))),
+    circularTask->positionTask->refAccel(Eigen::Vector3d(0, -R_*R_*omega_*std::sin(omega_*ctlTime_), R_*R_*omega_*std::cos(omega_*ctlTime_))), 
+    circularTask->orientationTask->orientation(Eigen::Quaterniond(0, 1, 0, 1).normalized().toRotationMatrix());}
+
+  auto ctrl_mode = datastore().get<std::string>("ControlMode");
+
+  if(ctrl_mode.compare("Position") == 0)
+  {
+    return mc_control::MCController::run(mc_solver::FeedbackType::OpenLoop);
+  }
+  else
+  {
+    return mc_control::MCController::run(mc_solver::FeedbackType::ClosedLoopIntegrateReal);
+  }
+  return false;
 }
 
 void CircularController::reset(const mc_control::ControllerResetData & reset_data)
--- src/CircularController.h
+++ src/CircularController.h
@@ -1,7 +1,12 @@
 #pragma once
 
+#include <SpaceVecAlg/EigenTypedef.h>
+#include <SpaceVecAlg/SpaceVecAlg>
 #include <mc_control/mc_controller.h>
+#include <mc_tasks/EndEffectorTask.h>
+#include <mc_tasks/PostureTask.h>
 
+#include <memory>
 
 #include "api.h"
 
@@ -12,4 +17,15 @@ struct CircularController_DLLAPI CircularController : public mc_control::MCContr
   bool run() override;
 
   void reset(const mc_control::ControllerResetData & reset_data) override;
+
+  std::shared_ptr<mc_tasks::EndEffectorTask> circularTask;  
+  std::shared_ptr<mc_tasks::PostureTask> postureTask;
+
+
+private:
+  mc_rtc::Configuration config_;
+  bool start_moving_;
+  double ctlTime_;
+  double omega_;
+  double R_;
 };
